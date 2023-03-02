f9816e0cb4df69e08849cb2949b5d32370506f72
ollie <ojs.anorak2000@gmail.com>
2023-03-02 15:57:03 +0900

    alter loading animation

--- lolstats/static/js/timer.js
+++ lolstats/static/js/timer.js
@@ -1,27 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var timerVar = setInterval(countTimer, 1000);
-    let gameLength = JSON.parse(document.getElementById('gameLength').textContent);
-    var totalSeconds = parseInt(gameLength);
-    function countTimer() {
-        ++totalSeconds;
-        var hour = Math.floor(totalSeconds / 3600);
-        var minute = Math.floor((totalSeconds - hour * 3600) / 60);
-        var seconds = totalSeconds - (hour * 3600 + minute * 60);
-        if (hour < 10) {
-            hour = "0" + hour;
+    let timer = document.getElementById("timer");
+    if (timer) {
+        var timerVar = setInterval(countTimer, 1000);
+        let gameLength = JSON.parse(document.getElementById('gameLength').textContent);
+        var totalSeconds = parseInt(gameLength);
+        function countTimer() {
+            ++totalSeconds;
+            var hour = Math.floor(totalSeconds / 3600);
+            var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+            var seconds = totalSeconds - (hour * 3600 + minute * 60);
+            if (hour < 10) {
+                hour = "0" + hour;
+            }
+            if (minute < 10) {
+                minute = "0" + minute;
+            }
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+            if (hour > 0) {
+                timer.innerHTML = hour + ":" + minute + ":" + seconds;
+            }
+            else {
+                timer.innerHTML = minute + ":" + seconds;
+            }
+                
         }
-        if (minute < 10) {
-            minute = "0" + minute;
-        }
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        if (hour > 0) {
-            document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
-        }
-        else {
-            document.getElementById("timer").innerHTML = minute + ":" + seconds;
-        }
-            
     }
 });
